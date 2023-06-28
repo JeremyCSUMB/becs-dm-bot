a3b1c9ef6f8188883b8974cd523abb3c1a494dfd
JeremyCSUMB <jeshaw@csumb.edu>
2023-06-28 14:33:33 -0700

    Added some abstractions with the becs-dm-bot code

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# becs-dm-bot
+# Slack Progress Notifier
+
+This bot monitors a Google Sheet for students' progress in a particular subject and sends notifications to the students via Slack if they have not made any progress.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+- Python 3.x
+- Google account with access to Google Sheets
+- Slack Bot Token
+- Flask (for executing Python code from Slack)
+
+### Installation
+
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/jeremycsumb/becs-dm-bot.git
+
+2. Install the required packages:
+    pip install gspread google-auth slack-sdk Flask
+
+3. Setup Google Sheets API from Google Cloud Console and download the credentials.json file. Place this file in the root directory of the project.
+
+4. Set up a Slack bot and obtain a bot token.
--- dm-bot.py
+++ dm-bot.py
@@ -2,44 +2,44 @@ import gspread
 from google.oauth2.service_account import Credentials
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-
-SLACK_BOT_TOKEN = 'your_slack_bot_token_here'
-slack_client = WebClient(token=SLACK_BOT_TOKEN)
+from secrets import SLACK_BOT_TOKEN
 
 
-scopes = ['https://www.googleapis.com/auth/spreadsheets',
-          'https://www.googleapis.com/auth/drive']
-credentials = Credentials.from_service_account_file(
-    "credentials.json", scopes=scopes)
-client = gspread.authorize(credentials)
+def notify_users_about_progress(slack_bot_token, sheet_id, subject):
+    slack_client = WebClient(token=slack_bot_token)
 
-sheet_id = "your_sheet_id_here"
-sheet = client.open_by_key(sheet_id).worksheet("Sheet1")
+    scopes = ['https://www.googleapis.com/auth/spreadsheets',
+              'https://www.googleapis.com/auth/drive']
+    credentials = Credentials.from_service_account_file(
+        'credentials.json', scopes=scopes)
+    client = gspread.authorize(credentials)
+
+    sheet = client.open_by_key(sheet_id).worksheet('Sheet1')
+
+    records = sheet.get_all_records()
+
+    for row in records:
+        email = row['Personal Email']
+        no_of_no_progress = row['# of No Progress']
+
+        if int(no_of_no_progress) > 0:
+            try:
+                user = slack_client.users_lookupByEmail(email=email)
+                user_id = user['user']['id']
+            except SlackApiError as e:
+                print(f"Error looking up user by email: {e.response['error']}")
+                continue
+
+            message = f"Hello <@{user_id}>,\nYou have not made any progress in {subject} in the past week. Please take necessary actions."
+
+            try:
+                slack_client.chat_postMessage(channel=user_id, text=message)
+                slack_client.chat_postMessage(channel='#dm-bot-home', text=message)
+            except SlackApiError as e:
+                print(f"Error sending message: {e.response['error']}")
 
 
-records = sheet.get_all_records()
-
-
-for row in records:
-    first_name = row['First Name']
-    last_name = row['Last Name']
-    email = row['Personal Email']
-    programming = row['PROGRAMMING']
-    no_of_no_progress = row['# of No Progress']
-
-    if int(no_of_no_progress) > 0:
-
-        try:
-            user = slack_client.users_lookupByEmail(email=email)
-            user_id = user['user']['id']
-        except SlackApiError as e:
-            print(f"Error looking up user by email: {e.response['error']}")
-            continue
-
-        message = f"Hello <@{user_id}>,\nYou have not made any progress in programming in the past week. Please take necessary actions."
-
-        try:
-            slack_client.chat_postMessage(channel=user_id, text=message)
-            slack_client.chat_postMessage(channel='#dm-bot-home', text=message)
-        except SlackApiError as e:
-            print(f"Error sending message: {e.response['error']}")
+# Usage
+SHEET_ID = 'your_sheet_id'
+SUBJECT = 'your_subject'
+notify_users_about_progress(SLACK_BOT_TOKEN, SHEET_ID, SUBJECT)
